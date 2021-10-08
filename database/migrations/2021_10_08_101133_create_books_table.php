839cc3878d2d0c08e7d4440d51e59291e6275a01
DuyenNguyen66 <hongduyen.7111998@gmail.com>
2021-10-08 17:39:34 +0700

    Create Book table

---
+++ database/migrations/2021_10_08_101133_create_books_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('author', 60);
+            $table->tinyInteger('language');
+            $table->integer('pages');
+            $table->text('font_cover');
+            $table->text('back_cover');
+            $table->float('price');
+            $table->tinyInteger('published_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
